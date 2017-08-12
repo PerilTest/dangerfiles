321f0f801a6c64e6c933adbcd1d702aebefae044
Orta <orta.therox+github@gmail.com>
2017-08-12 19:12:04 -0400

    Update hello-world.ts

--- org/hello-world.ts
+++ org/hello-world.ts
@@ -4,11 +4,12 @@ markdown("👍")
 
 markdown(JSON.stringify(danger, null, "  ")
 
-// // Try updating the word 'danger' to 'DaNgEr' in issues.
-// const body = danger.github.issue.body
-// if (body.includes("danger")) {
-//   const newBody = body.replace(/danger/, "DaNgEr")
-//   schedule(async () => {
-//     await danger.github.api.issues.edit({ ...danger.github.thisPR, body: newBody })
-//   })
-// }
+// Try updating the word 'danger' to 'DaNgEr' in issues.
+const body = danger.github.issue.body
+if (body.includes("danger")) {
+  const newBody = body.replace(/danger/, "DaNgEr")
+  warning("Changed word")
+  schedule(async () => {
+    await danger.github.api.issues.edit({ ...danger.github.thisPR, body: newBody })
+  })
+}
