010a2e5046b8e35f00804fe820a28228d75fb5c2
Orta <orta.therox+github@gmail.com>
2018-09-01 11:52:31 -0400

    Update spellcheck.ts

--- org/spellcheck.ts
+++ org/spellcheck.ts
@@ -1,3 +1,5 @@
 import spellcheck from 'danger-plugin-spellcheck'
+import {warning} from "danger"
 
+warning("This looks like a great PR")
 schedule(spellcheck({ settings: "artsy/artsy-danger@spellcheck.json" }))
